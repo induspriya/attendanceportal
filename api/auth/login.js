5a5de08ae64eccaa020d10992b9deeb53bdec634
induspriya <priya191177@gmail.com>
2025-08-14 21:36:53 +0530

    Fix API routing and CORS for Vercel deployment

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,7 +1,7 @@
 module.exports = async (req, res) => {
-  // Set CORS headers
+  // Set CORS headers for Vercel deployment
   res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
