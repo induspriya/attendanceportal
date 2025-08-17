61af5d242ca7341cca35678b158f4697ed02fbb5
induspriya <priya191177@gmail.com>
2025-08-17 16:38:33 +0530

    Remove remaining model imports from holidays and news APIs

--- api/holidays/upcoming.js
+++ api/holidays/upcoming.js
@@ -1,19 +1,4 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-
-// Import models
-const Holiday = require('../models/Holiday');
-
-// Connect to MongoDB
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGODB_URI);
-    console.log('MongoDB connected');
-  } catch (error) {
-    console.error('MongoDB connection error:', error);
-    throw error;
-  }
-};
+// Mock data endpoint - no database connection needed
 
 module.exports = async (req, res) => {
   // Set CORS headers
