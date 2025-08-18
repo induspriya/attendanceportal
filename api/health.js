c1b91fbcf04f4cda203c011eff8956610ca6903a
induspriya <priya191177@gmail.com>
2025-08-18 21:59:45 +0530

    Fix health.js API route - Convert CommonJS to ES6 syntax for Vercel compatibility

--- api/health.js
+++ api/health.js
@@ -1,4 +1,4 @@
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
   // Set CORS headers for Vercel deployment
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
@@ -30,4 +30,4 @@ module.exports = async (req, res) => {
       error: error.message 
     });
   }
-};
+}
